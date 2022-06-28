d2b62c88fb5a9146402db121fc8878451ec81ce3
Graham Billey <grahambilley@gmail.com>
2022-06-28 16:24:01 -0400

    Update motor_functions.py

--- motor_functions.py
+++ motor_functions.py
@@ -1,1 +1,10 @@
 # This .py file contains the function definitions for controlling the stepper motors.
+
+from config import MOTOR_PINS
+
+
+def move_motor(XYZ, distance, resolution='Full', travel_time=1, wait_time=0.5):
+    pins=MOTOR_PINS[XYZ]
+    DIR=pins['DIR']
+    STEP=pins['STEP']
+    MODE=pins['MODE']
