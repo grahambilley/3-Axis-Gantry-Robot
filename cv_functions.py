126fc8f63b04828c4d5d9e8bff496bb0ac7fe62d
Graham Billey <grahambilley@gmail.com>
2022-06-28 16:24:19 -0400

    Update cv_functions.py

--- cv_functions.py
+++ cv_functions.py
@@ -1,3 +1,9 @@
 # This .py file contains functions related to computer vision and image processing.
 
+def pixels_to_mm(pix):
+    return pix  # Need to define thr relationship
+
+def mm_to_pixels(mm):
+    return mm  # Need to define thr relationship
+
 
