66f2cf01d264dd18d7d40fcf946df0c57c2cbb99
Graham Billey <grahambilley@gmail.com>
2022-07-05 13:40:08 -0400

    Update main.py
    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # INITIALIZATION
-from cv_functions import take_picture, findAruco, ResizeWithAspectRatio, find_carts, pixels_to_mm, mm_to_pixels
+from cv_functions import make_dispensing_plan, take_picture, findAruco, ResizeWithAspectRatio, find_carts, pixels_to_mm, mm_to_pixels
+from motor_functions import move_motor, move_to_home
 
 
 
@@ -8,19 +9,19 @@ from cv_functions import take_picture, findAruco, ResizeWithAspectRatio, find_ca
 # TAKE PICTURE OF CART TRAY
 carts = take_picture()
 carts_compressed = ResizeWithAspectRatio(carts, width=1000)
-circles, dispense_locations = find_carts(carts_compressed)
+circles, dispense_locations, _ = find_carts(carts_compressed)
 
 
 
 # CREATE MOVEMENT PLAN
-
+dispensing_plan = make_dispensing_plan(dispense_locations)
 
 
 
 
 
 # MOVE ROBOT TO FILL CARTS
-
+move_to_home
 
 
 
