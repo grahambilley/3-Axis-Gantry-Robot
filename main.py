f72cd40e276110fdca4885cd66a366be92c6d3b0
Graham Billey <grahambilley@gmail.com>
2022-07-05 10:37:58 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+# INITIALIZATION
+from cv_functions import take_picture, findAruco, ResizeWithAspectRatio, find_carts, pixels_to_mm, mm_to_pixels
+
+
+
+
+
+# TAKE PICTURE OF CART TRAY
+
+
+
+
+
+
+# CREATE MOVEMENT PLAN
+
+
+
+
+
+
+# MOVE ROBOT TO FILL CARTS
+
+
+
+
+
+
+
