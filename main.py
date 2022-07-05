d9f10ae84c4f75aba0c58863e26fe9aa331a0fc0
grahambilley <55400403+grahambilley@users.noreply.github.com>
2022-07-05 15:27:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ imagePath = f'./Pictures/{imgName}'
 img = cv2.imread(imagePath,0)
 
 print('Locating cart edges...')
-circles, dispense_locations, _ = find_carts(img)
+circles, dispense_locations, bbox, ids = find_carts(img)
 
 # CREATE MOVEMENT PLAN
 print('Creating dispensing plan...')
@@ -21,14 +21,19 @@ dispensing_plan = make_dispensing_plan(dispense_locations)
 # MOVE ROBOT TO FILL CARTS
 print('Moving home...')
 move_to_home()
+
 print('Executing dispensing plan...')
 for movement in dispensing_plan:
-    move_motor('X', movement[0])
-    move_motor('Y', movement[1])
-    move_motor('Z',5)
+    #print('moving x')
+    move_motor('X', movement[0]/10, resolution='1/32')
+    #print('moving y')
+    move_motor('Y', movement[1]/10, resolution='1/32')
+    #print('moving z 1')
+    move_motor('Z',15, resolution='1/32')
     # Dispense....
-    move_motor('Z',-5)
+    #print('moving z 2') 
+    move_motor('Z',-15, resolution='1/32')
+
 print('Moving home...')
 move_to_home()
-
 print('Filling Complete!')
